0785cba979ec5e86dad304f17785ba944b344978
VictorHugo25 <victorhugoar50@gmail.com>
2025-10-08 17:11:13 -0300

    fix(productItem): fix

--- src/components/main/ProductItem1/index.tsx
+++ src/components/main/ProductItem1/index.tsx
@@ -2,37 +2,57 @@ import { Text, TouchableOpacity, View, Image, Pressable } from "react-native";
 import React from "react";
 import { styles } from "./style";
 import { router } from "expo-router";
+import RatingReadOnly from "../../productDetail/avaliacoes";
+import colors from "@/src/app/styles/colors";
+import { TextQuiche } from "../../quiche";
+import { QuicheBold } from "../../quiche/quiche-bold";
 
 export default function ProductItem1({
+  id,
   nome,
   preco,
   precoComDesconto,
   imagem,
+  newStyles,
+  productItemStyle,
 }: {
+  id: number;
   nome: string;
   preco: number;
   precoComDesconto: number;
   imagem: string;
+  newStyles?: object;
+  productItemStyle?: object;
 }) {
   return (
-    <View style={styles.container}>
-      <Pressable style={styles.ProductItem}>
+    <View style={styles.container && newStyles}>
+      <Pressable
+        style={[styles.ProductItem, productItemStyle]}
+        onPress={() => router.push(`/productDetail/${id}`)}
+      >
         <View style={styles.content}>
-          {" "}
           {/* <- AGORA COM flex: 1 */}
           <Image
+            // source={{
+            //   uri: `http://192.168.1.2:8080/produtos/imagens/${imagem}`,
+            // }}
             source={require("../../../../assets/images/violãoGewa.png")}
             style={styles.image}
             resizeMode="contain"
           />
-          <Text style={styles.title} numberOfLines={2}>
+
+          <RatingReadOnly value={5} size={10} />
+
+          <QuicheBold style={styles.title} numberOfLines={2}>
             {nome}
-          </Text>
+          </QuicheBold>
           <View style={styles.infoContainer}>
-            <Text style={styles.price}>{`R$ ${preco}`}</Text>
-            <Text
+            <TextQuiche style={styles.price}>{`R$${preco.toFixed(
+              2
+            )}`}</TextQuiche>
+            <QuicheBold
               style={styles.precoComDesconto}
-            >{`R$ ${precoComDesconto}`}</Text>
+            >{`R$${precoComDesconto.toFixed(2)}`}</QuicheBold>
           </View>
         </View>
       </Pressable>
