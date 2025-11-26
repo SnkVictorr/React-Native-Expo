ff92f6caf05ceedd03c166646bed1e676149d1ea
SnkVictorr <victorhugoar50@gmail.com>
2025-11-25 23:47:05 -0300

    busca de produtos(inicio)

---
+++ src/app/services/Search/get.tsx
@@ -0,0 +1,28 @@
+import { BASE_URL, AUTH_TOKEN } from "../../config/api";
+
+export default async function buscarProdutos(query: string) {
+  try {
+    const response = await fetch(
+      `${BASE_URL}/produtos/?q=${encodeURIComponent(query)}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: AUTH_TOKEN,
+        },
+      }
+    );
+    const json = await response.json();
+
+    if (json.status === "success") {
+      return json.data;
+    } else {
+      console.error("Erro na resposta da API:", json.message);
+      return [];
+    }
+  } catch (error) {
+    console.error("Erro na busca:", error);
+  } finally {
+    console.log("Busca finalizada.");
+  }
+}
--- src/components/searchBar/index.tsx
+++ src/components/searchBar/index.tsx
@@ -1,9 +1,34 @@
-import { Text, TextInput, View } from "react-native";
-import React, { Component } from "react";
+import {
+  FlatList,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { Component, useEffect, useState } from "react";
 import styles from "./style";
 import { Ionicons } from "@expo/vector-icons";
-
+import buscarProdutos from "@/src/app/services/Search/get";
+import { Produto } from "@/src/app/types/produto";
 export default function SearchBar() {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState<Produto[]>([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    async function fetchData() {
+      if (query.trim().length === 0) {
+        setResults([]);
+        return;
+      }
+
+      setLoading(true);
+      const data = await buscarProdutos(query);
+      setResults(data || []);
+      setLoading(false);
+    }
+
+    fetchData();
+  }, [query]);
   return (
     <View>
       {/* Search Bar */}
@@ -13,10 +38,31 @@ export default function SearchBar() {
             placeholder="Buscar produtos..."
             placeholderTextColor="#888"
             style={styles.searchInput}
+            onChange={(e) => setQuery(e.nativeEvent.text)}
           />
           <Ionicons name="search" size={20} color="#888" />
         </View>
       </View>
+      {/* LISTA DE RESULTADOS */}
+      {query.length > 0 && (
+        <View
+          style={{ marginTop: 10, backgroundColor: "#fff", borderRadius: 8 }}
+        >
+          {loading ? (
+            <Text style={{ padding: 10 }}>Carregando...</Text>
+          ) : (
+            <FlatList
+              data={results}
+              keyExtractor={(item) => item.id_produto.toString()}
+              renderItem={({ item }) => (
+                <TouchableOpacity style={{ padding: 12 }}>
+                  <Text style={{ fontSize: 16 }}>{item.produto}</Text>
+                </TouchableOpacity>
+              )}
+            />
+          )}
+        </View>
+      )}
     </View>
   );
 }
